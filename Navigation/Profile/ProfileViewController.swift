ac1af8a69b74244258672fac642a4bcaec7c8e9c
Elena Shagaeva <lpraud@mail.ru>
2021-01-12 10:48:55 +0300

    new version

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -22,9 +22,8 @@ class ProfileViewController: UIViewController {
         view.backgroundColor = .lightGray
         view.addSubview(header)
         
-        // FIX ME
         header.avatarImage.frame = CGRect(x: 16, y: 84, width: 100, height: 100)
-//        header.titleLabel.frame = CGRect(x: view.center.x / 2, y: view.safeAreaInsets.bottom + 47 , width: 100, height: 20)
+        header.titleLabel.frame = CGRect(x: view.center.x, y: 100 , width: 100, height: 20)
         header.showButton.frame = CGRect(x: header.avatarImage.frame.origin.x, y: header.avatarImage.frame.width + 100, width: view.frame.width - 32, height: 50)
     }
 }
